3a3702a0dc2fda05afc362325de6ef319feb8d72
BorowiecK2018 <kborowiec@sjnma.org>
2018-05-18 09:12:46 -0500

    javascript added

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Hello welcome to Madlib");
+
+
+var noun1 = prompt("Please enter a noun","");
+var noun2 = prompt("Please enter a noun","");
+var verb1 = prompt("Please enter a noun","");
+var verb2 = prompt("Please enter a noun","");
+var adjective1 = prompt("Please enter a noun","");
+var adjective2 = prompt("Please enter a noun","");
+
+document.getElementById("name").innerHTML =  "The" + noun1 +"walked to the" + noun2 "The" + adjective1 + "Dog is also" + adjective2 "The dog" +verb1 + "on the tree." "The dog" + verb2  "the cat."";
